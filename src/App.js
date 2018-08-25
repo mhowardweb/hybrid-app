e94d282ecba58a2515e425e03db46f72328b64a9
mhowardweb <mhoward181@outlook.com>
2018-08-25 18:56:49 +0100

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-export default class App extends Component {
+import React from "react";
+import {
+  View,
+  Text,
+  Animated,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity
+} from "react-native";
+import { Router, Switch, Link, Route } from "./Routing";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageAnimation = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    Animated.loop(
+      Animated.timing(this.imageAnimation, {
+        toValue: 1,
+        duration: 1005
+      })
+    ).start();
+
+    StatusBar.setBarStyle("light-content");
+  }
+
+  Home = rotationStyle => {
+    return (
+      <View style={{ alignItems: "center", flex: 3 }}>
+        <Link to={"/one"} component={TouchableOpacity}>
+          <Text style={styles.appIntro}>
+            To get started, edit src/App.js and save to reload.
+          </Text>
+        </Link>
+      </View>
+    );
+  };
   render() {
+    const rotationStyle = {
+      transform: [
+        {
+          rotate: this.imageAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: ["0deg", "360deg"]
+          })
+        }
+      ]
+    };
+
     return (
       <View style={styles.app}>
         <View style={styles.appHeader}>
-          <Text style={styles.appTitle}>Welcome to React ⚛️</Text>
+          <Animated.Image
+            style={[styles.headerImage, rotationStyle]}
+            resizeMode={"contain"}
+            source={require("./assets/react-logo.png")}
+          />
+          <Text style={styles.appTitle}>Welcome to React Native Web️</Text>
+          <Text style={styles.appSubtitle}>Navigation Edition</Text>
         </View>
-        <Text style={styles.appIntro}>
-          To get started, edit src/App.js and save to reload.
-        </Text>
+        <Router>
+          <Switch hideNavBar={true}>
+            <Route exact path="/" component={this.Home} />
+            <Route path="/one" component={Other} />
+          </Switch>
+        </Router>
       </View>
     );
   }
 }
+
+export class Other extends React.Component {
+  render() {
+    return (
+      <View style={{ alignItems: "center", flex: 3 }}>
+        <Link to={"/"} component={TouchableOpacity}>
+          <Text style={styles.appIntro}>Other page</Text>
+        </Link>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   app: {
     flex: 1
@@ -25,12 +95,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
+  headerImage: {
+    width: 200,
+    height: 200,
+    flex: 3
+  },
   appTitle: {
+    flex: 1,
     fontSize: 16,
     color: "white"
   },
+  appSubtitle: {
+    color: "white"
+  },
   appIntro: {
-    flex: 2,
+    flex: 3,
     fontSize: 30,
     textAlign: "center"
   }
